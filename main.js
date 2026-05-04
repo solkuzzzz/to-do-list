36b4fb869a2c06fd561b77199ee2bbd710715510
Sofia Vlasova <solkzkreativ@mail.ru>
2026-05-04 12:47:19 +0300

    add some features

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-const array = ['good', 'better', 'the best', 'good']
+const todoList = []
 
-function countWords(array) {
-  const result = {};
-  
-  for (let i = 0; i < array.length; i++) {
-    const word = array[i]
-    /*result[`${array[i]}`] = array.filter(el => el === array[i]).length*/
-    if (!result[word]) {
-      result[word] = 1
-    } else {
-      result[word] ++
-    }
+renderTodoList()
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i]
+    const { name, dueDate } = todoObject;
+
+    const html = `
+      <div class="name-container">
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        " class="delete-button">Delete</button> </div>
+      `;
+        todoListHTML += html;
   }
-  return result;
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
-console.log(countWords(array))
+function clickEnter(event) {
+  if (event.key === 'Enter') {
+    addTodo()
+  }
+}
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-input')
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({ name, dueDate })
+
+  inputElement.value = '';
+
+  renderTodoList();
+
+}
